142c16960e5f6fd237870ef205f789c022fd1fdb
Stan <stancai@dianhun.cn>
2021-01-08 12:02:20 +0800

    - refactored tutorial asset;

---
+++ Assets/Scripts/Tutorial/OneWeekend/OneWeekendTutorialAsset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OneWeekendTutorialAsset", menuName = "Rendering/OneWeekendTutorialAsset")]
+public class OneWeekendTutorialAsset : RayTracingTutorialAsset
+{
+	public string tutorialName;
+
+	public override RayTracingTutorial createTutorial()
+	{
+		RayTracingTutorial ret = null;
+
+		switch (tutorialName)
+		{
+			default:
+			case "OutputColorTutorial": ret = new OutputColorTutorial(this); break;
+			case "BackgroundTutorial": ret = new BackgroundTutorial(this); break;
+		}
+
+		return ret;
+	}
+}
